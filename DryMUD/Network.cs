7b2e3241a3a6e552fd97b8c713238b0a5c4ebd82
mvvalt <waltematem@gmail.com>
2020-07-24 09:30:07 -0500

    Asynchronous connection listener, send buffer ticking

--- DryMUD/Network.cs
+++ DryMUD/Network.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
@@ -9,7 +10,7 @@ namespace Network
     class Connection
     {
         public const int receive_buffer_size = 1024;
-        public const int send_buffer_size = 3 * 1024;
+        public const int send_buffer_size = 4; // @DEV
 
 
         public Socket socket = null;
@@ -17,56 +18,65 @@ namespace Network
         public byte[] receive_buffer_data = new byte[receive_buffer_size];
         public StringBuilder receive_buffer_string = new StringBuilder();
 
-        public byte[] send_buffer_data = new byte[send_buffer_size];
         public StringBuilder send_buffer_string = new StringBuilder();
     }
 
 
     class ConnectionHandler
-    {
-        public const int listener_port = 4000;
+    {   
         public const int connection_queue_size = 100;
-        public static ManualResetEvent manual_reset_event = new ManualResetEvent(false);
 
+        private static int listener_port;
+
+        private static readonly object lock_object = new object();
+        private static Dictionary<Socket, Connection> socket_to_connection = new Dictionary<Socket, Connection>();
+        private static Socket listener;
+        
         public ConnectionHandler()
-        {
+        {   
         }
 
-        public static void Listen()
+        public static void Start(int port)
         {
+            listener_port = port;
+
             IPHostEntry ip_host_info = Dns.GetHostEntry("localhost"); // Dns.GetHostEntry(Dns.GetHostName());
             IPAddress ip_address = ip_host_info.AddressList[0];
             IPEndPoint local_end_point = new IPEndPoint(ip_address, listener_port);
 
-            Socket listener = new Socket(ip_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
-
+            listener = new Socket(ip_address.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
 
             listener.Bind(local_end_point);
             listener.Listen(connection_queue_size);
-
-            while (true)
-            {
-                manual_reset_event.Reset();
-
-                Console.WriteLine("Waiting for connection on port {0}...", listener_port);
-                listener.BeginAccept(new AsyncCallback(AcceptCallback), listener);
-
-                manual_reset_event.WaitOne();
-            }
+            listener.BeginAccept(new AsyncCallback(AcceptCallback), null);
         }
 
         public static void AcceptCallback(IAsyncResult async_result)
         {
-            manual_reset_event.Set();
-
             Console.WriteLine("Incoming connection.");
 
-            Socket listener = (Socket)async_result.AsyncState;
-            Socket handler = listener.EndAccept(async_result);
+            Socket socket = listener.EndAccept(async_result);
 
             Connection client = new Connection();
-            client.socket = handler;
-            handler.BeginReceive(client.receive_buffer_data, 0, Connection.receive_buffer_size, 0, new AsyncCallback(ReadCallback), client);
+            client.socket = socket;
+
+            lock (lock_object)
+            {
+                socket_to_connection.Add(socket, client);
+            }
+
+            try
+            {
+                socket.BeginReceive(client.receive_buffer_data, 0, Connection.receive_buffer_size, 0, new AsyncCallback(ReadCallback), client);
+                // @DEV
+                Send(socket, "This is a lot of text to test the metered output of FlushSendBuffers().");
+            }
+            catch(SocketException)
+            {
+                Disconnect(socket);
+            }
+
+            listener.BeginAccept(new AsyncCallback(AcceptCallback), null);
         }
 
         public static void ReadCallback(IAsyncResult async_result)
@@ -74,42 +84,96 @@ namespace Network
             string content = String.Empty;
 
             Connection client = (Connection)async_result.AsyncState;
-            Socket handler = client.socket;
+            Socket socket = client.socket;
 
-            int bytes_read = handler.EndReceive(async_result);
-            if (bytes_read > 0)
+            try
             {
-                client.receive_buffer_string.Append(Encoding.ASCII.GetString(client.receive_buffer_data, 0, bytes_read));
-
-                // Check the input buffer for a line-terminated command
-                content = client.receive_buffer_string.ToString();
-                int line_terminator_index = content.IndexOf('\n');
-                if (line_terminator_index >= 0)
+                int bytes_read = socket.EndReceive(async_result);
+                if (bytes_read > 0)
                 {
-                    string command = content.Substring(0, line_terminator_index);
-                    string new_receive_buffer_data = content.Substring(line_terminator_index + 1);
-                    client.receive_buffer_string = new StringBuilder(new_receive_buffer_data);
+                    client.receive_buffer_string.Append(Encoding.ASCII.GetString(client.receive_buffer_data, 0, bytes_read));
 
-                    Console.WriteLine("Command: {0}", command);
-                    Send(handler, command);
+                    // Check the input buffer for a line-terminated command
+                    content = client.receive_buffer_string.ToString();
+                    int line_terminator_index = content.IndexOf('\n');
+                    if (line_terminator_index >= 0)
+                    {
+                        string command = content.Substring(0, line_terminator_index);
+                        string new_receive_buffer_data = content.Substring(line_terminator_index + 1);
+                        client.receive_buffer_string = new StringBuilder(new_receive_buffer_data);
+
+                        Console.WriteLine("Command: {0}", command);
+                        Send(socket, command); // @DEV
+                    }
+
+                    socket.BeginReceive(client.receive_buffer_data, 0, Connection.receive_buffer_size, 0, new AsyncCallback(ReadCallback), client);
                 }
-
-                handler.BeginReceive(client.receive_buffer_data, 0, Connection.receive_buffer_size, 0, new AsyncCallback(ReadCallback), client);
+            }
+            catch(SocketException)
+            {
+                Disconnect(socket);
             }
         }
 
-        private static void Send(Socket handler, String data)
+        public static void Send(Socket socket, string data)
         {
-            byte[] byte_data = Encoding.ASCII.GetBytes(data);
-            handler.BeginSend(byte_data, 0, byte_data.Length, 0, new AsyncCallback(SendCallback), handler);
+            Connection client = socket_to_connection[socket];
+            client.send_buffer_string.Append(data);
+
+            if (client.send_buffer_string.Length > Connection.send_buffer_size)
+            {
+                // @TODO: Send buffer is getting too large, do something about it
+            }
+        }
+
+        public static void FlushSendBuffers()
+        {
+            foreach(Connection client in socket_to_connection.Values)
+            {
+                if (client.send_buffer_string.Length > 0)
+                {
+                    int len = Connection.send_buffer_size;
+                    if (client.send_buffer_string.Length < Connection.send_buffer_size)
+                    {
+                        len = client.send_buffer_string.Length;
+                    }
+
+                    byte[] byte_data = Encoding.ASCII.GetBytes(client.send_buffer_string.ToString(0, len));
+                    client.send_buffer_string.Remove(0, len);
+
+                    try
+                    {
+                        client.socket.BeginSend(byte_data, 0, byte_data.Length, 0, new AsyncCallback(SendCallback), client.socket);
+                    }
+                    catch (SocketException)
+                    {
+                        Disconnect(client.socket);
+                    }
+                }
+            }
         }
 
         private static void SendCallback(IAsyncResult async_result)
         {
-            Socket handler = (Socket)async_result.AsyncState;
+            Socket socket = (Socket)async_result.AsyncState;
 
-            int bytes_sent = handler.EndSend(async_result);
+            int bytes_sent = socket.EndSend(async_result);
             Console.WriteLine("Send {0} bytes to client.", bytes_sent);
+        }
+
+        public static void Disconnect(Socket socket)
+        {
+            // @TODO: go through any game data lists and see if they are controlled by this connection
+
+            lock (lock_object)
+            {
+                socket_to_connection.Remove(socket);
+            }
+
+            Console.WriteLine("Socket disconnected.");
+
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
         }
     }
 }
--- DryMUD/Program.cs
+++ DryMUD/Program.cs
@@ -1,10 +1,58 @@
 ﻿using System;
+using System.Diagnostics;
 
-class Program
+namespace DryMUD
 {
-    static void Main(string[] args)
+    class Timer
     {
-        Console.WriteLine("Starting network handler.");
-        Network.ConnectionHandler.Listen();
+        private readonly double tick_ms;
+        private Stopwatch stopwatch = new Stopwatch();
+        private double elapsed_us;
+
+        public Timer(long tick_length_ms)
+        {
+            tick_ms = (double)tick_length_ms;
+            elapsed_us = 0;
+        }
+
+        public bool CheckTick()
+        {
+            stopwatch.Stop();
+            elapsed_us += ((double)stopwatch.ElapsedTicks / Stopwatch.Frequency) * 1000000.0;
+            stopwatch.Restart();
+
+            if (elapsed_us >= (tick_ms * 1000.0))
+            {
+                elapsed_us -= (tick_ms * 1000.0);
+                return true;
+            }
+
+            return false;
+        }
     }
+
+    class Program
+    {
+        private static bool running = true;
+
+        static void Main(string[] args)
+        {
+            const int port = 4000;
+            const int tick_flush_network_buffers_ms = 1000;
+
+            Console.WriteLine("Starting network handler.");
+            Network.ConnectionHandler.Start(port);
+
+            Timer timer_flush_network_buffers = new Timer(tick_flush_network_buffers_ms);
+
+            while (running)
+            {
+                if (timer_flush_network_buffers.CheckTick())
+                {
+                    Network.ConnectionHandler.FlushSendBuffers();
+                }
+            }
+        }
+    }
+
 }
